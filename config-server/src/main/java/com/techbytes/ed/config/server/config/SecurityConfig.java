2548a0f4cb8f1b58dc9ef0af253bf73bd147f8b5
Santosh Kumar Pokhyan <santc2002@live.com>
2022-11-23 19:49:09 -0500

    spring cli, spring cloud cli and ICE encryption functionality added

---
+++ config-server/src/main/java/com/techbytes/ed/config/server/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.techbytes.ed.config.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity webSecurity) throws Exception{
+        webSecurity.ignoring()
+                .antMatchers("/encrypt/**")
+                .antMatchers("/decrypt/**");
+        super.configure(webSecurity);
+    }
+}

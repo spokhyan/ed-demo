2288b51e68a5bd6bf967d6205d5a84e314cdb77e
Santosh Kumar Pokhyan <santc2002@live.com>
2022-11-30 11:40:44 -0500

    elastic query web client

--- elastic-query/src/main/java/com/techbytes/ed/elastic/query/controller/ElasticDocumentController.java
+++ elastic-query/src/main/java/com/techbytes/ed/elastic/query/controller/ElasticDocumentController.java
@@ -2,6 +2,7 @@ package com.techbytes.ed.elastic.query.controller;
 
 import com.techbytes.ed.elastic.query.model.ElasticQueryServiceRequestModel;
 import com.techbytes.ed.elastic.query.model.ElasticQueryServiceResponseModel;
+import com.techbytes.ed.elastic.query.model.ElasticQueryServiceResponseModelV2;
 import com.techbytes.ed.elastic.query.service.ElasticQueryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,7 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/documents")
+@RequestMapping(value = "/documents", produces = "application/vnd.api.v1+json")
 public class ElasticDocumentController {
     private static final Logger LOG = LoggerFactory.getLogger(ElasticDocumentController.class);
 
@@ -42,6 +43,16 @@ public class ElasticDocumentController {
         return ResponseEntity.ok(elasticQueryServiceResponseModel);
     }
 
+    @GetMapping(value = "/{id}", produces = "application/vnd.api.v2+json")
+    public @ResponseBody
+    ResponseEntity<ElasticQueryServiceResponseModelV2>
+    getDocumentByIdV2(@PathVariable @NotEmpty String id) {
+        ElasticQueryServiceResponseModel elasticQueryServiceResponseModel = elasticQueryService.getDocumentById(id);
+        ElasticQueryServiceResponseModelV2 responseModelV2 = getV2Model(elasticQueryServiceResponseModel);
+        LOG.debug("Elasticsearch returned document with id {}", id);
+        return ResponseEntity.ok(responseModelV2);
+    }
+
     @PostMapping("/get-document-by-text")
     public @ResponseBody
     ResponseEntity<List<ElasticQueryServiceResponseModel>>
@@ -57,5 +68,16 @@ public class ElasticDocumentController {
         return ResponseEntity.ok(response);
     }
 
+    private ElasticQueryServiceResponseModelV2 getV2Model(ElasticQueryServiceResponseModel responseModel) {
+        ElasticQueryServiceResponseModelV2 responseModelV2 = ElasticQueryServiceResponseModelV2.builder()
+                .id(Long.parseLong(responseModel.getId()))
+                .userId(responseModel.getUserId())
+                .text(responseModel.getText())
+                .text2("Version 2 text")
+                .build();
+        responseModelV2.add(responseModel.getLinks());
+        return responseModelV2;
+
+    }
 
 }
